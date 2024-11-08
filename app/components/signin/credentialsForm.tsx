b2a139e875212892a76f7209c548ee354cf81e95
Max Mat <mmatronin@gmail.com>
2024-11-08 13:05:02 +0000

    Switched unused variables to warn

--- app/components/signin/credentialsForm.tsx
+++ app/components/signin/credentialsForm.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 export default function CredentialsForm() {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -31,9 +32,24 @@ export default function CredentialsForm() {
     onSubmit={handleSubmit}
     >
     
-    {error && <span className="text-red-500 font-semibold inline-block">{error}</span>}
-    <input type="email" name="email" placeholder="Email" required className="mb-4 p-2 border border-gray-300 rounded-md w-full"/>
-    <input type="password" name="password" placeholder="Password" required className="mb-4 p-2 border border-gray-300 rounded-md w-full"/>
+    {error && <span className="mb-1 text-red-500 font-semibold inline-block">{error}</span>}
+    <input type="email" name="email" placeholder="Email" required className="mb-4 p-2 border border-gray-300 rounded-md w-full text-black"/>
+    <div className="relative w-full mb-4">
+      <input 
+        type={showPassword ? "text" : "password"} 
+        name="password" 
+        placeholder="Password" 
+        required 
+        className="p-2 border border-gray-300 rounded-md w-full text-black"
+      />
+      <button
+        type="button"
+        className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? "🫥" : "👁️"}
+      </button>
+    </div>
       <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Sign In</button>
     </form>
   );
