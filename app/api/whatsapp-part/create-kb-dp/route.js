a1237a27ed7c46db72f7d118950e66524e4231f7
Max Mat <mmatronin@gmail.com>
2024-11-13 21:48:52 +0000

    Lets smash it!

--- app/api/whatsapp-part/create-kb-dp/route.js
+++ app/api/whatsapp-part/create-kb-dp/route.js
@@ -5,7 +5,7 @@ import createK8sDeployment from '@/lib/whatsAppService/kubernetes_part';
 export async function POST(request) {
     const { uniqueId } = await request.json();
     console.log("Started Creating client with uniqueId: ", uniqueId);
-    const success = await triggerDockerWhatsAppClientCreation(uniqueId);
+    const success = await createK8sDeployment(uniqueId);
     
     if (success) {
         return NextResponse.json(
