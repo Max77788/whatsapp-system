a82c2f066fcbae26688cd6b67b6ee9ea52e5bc62
Max Mat <mmatronin@gmail.com>
2024-11-15 14:36:29 +0000

    Cool stuff

--- app/api/leads/register/route.js
+++ app/api/leads/register/route.js
@@ -3,17 +3,18 @@ import { update_user } from '@/lib/utils';
 
 export async function POST(req) {
 
-    const { name, email } = await req.json();
+    const { name, email, phone_number } = await req.json();
     
     const { searchParams } = new URL(req.url);
     const unique_id = searchParams.get('unique_id');
+    const source = searchParams.get('source', "unknown");
+
     console.log(`unique_id: ${unique_id}`);
 
-    const success = await update_user({ unique_id: unique_id }, { leads: { name, email } }, "$addToSet");
+    const success = await update_user({ unique_id: unique_id }, { leads: { name, email, phone_number, source: source } }, "$addToSet");
     
     if (success) {
         return NextResponse.json({ message: "Facebook lead added" });
     } else {
-        return NextResponse.json({ message: "Failed to receive message" });
     }
 }
